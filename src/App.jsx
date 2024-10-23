7cac3f4d1cb54959456309bb7bded9162e7d561c
Sergio Arias <s-andres140@hotmail.com>
2024-10-23 06:29:05 -0500

    5.7 Blog List Frontend, step 7

--- src/App.jsx
+++ src/App.jsx
@@ -134,7 +134,7 @@ const App = () => {
         <h2>blogs list created by {user.name}</h2>
         {blogs && 
         (blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog} name={user.name} />
         ))
         }
       </>
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog, name }) => {
+  const [showDetails, setShowDetails] =useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  //cambia el estado showDetails para mostrar los demas atributos de blog
+  const controlShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return(
+  <div style={blogStyle}>
+    {blog.title} {blog.author} <button onClick={controlShowDetails}>view</button>
+    {showDetails && 
+    <div>
+    {blog.url}
+    <br/>
+    likes {blog.likes} <button>like</button>
+    <br/>
+    {name}
+    </div>
+    }
   </div>  
-)
+)}
 
 export default Blog
